49da84a72e486fc8da7361134b689c27b9edc0d3
Brooona <61800026+Brooona@users.noreply.github.com>
2023-01-04 10:17:59 +0100

    Update last_weeks_of_month.py
    returning current year, current month, max week number and current week. 
    Update: adding if case for first month of the year.

--- last_weeks_of_month.py
+++ last_weeks_of_month.py
@@ -10,18 +10,30 @@ def max_week_return():
     day = today.day
     weeks = []
 
+    curr_week = str(today.isocalendar()[1])
+    curr_week = "2023" + curr_week
+
     for i in range(1, calendar.monthrange(year, month)[1]):
+        print(i)
         datem = datetime.date(year, month, i)
         weeks.append(int(datem.isocalendar()[1]))
-
     df = pd.DataFrame(weeks, columns=["weeks"])
+    if month == 1:
+        #delete row with max week index
+
+        df.drop(df['weeks'].idxmax(), inplace=True)
+
+
     df = pd.DataFrame(df['weeks'].value_counts())
     df.reset_index(inplace=True)
     max_week = df['index'].max()
     max_week_prev = df['index'].max() - 1
+
     if df[df['index'] == max_week].iloc[0, 1] > df[df['index'] == max_week_prev].iloc[0, 1]:
         max_week_number = df[df['index'] == max_week].iloc[0, 0]
     else:
         max_week_number = df[df['index'] == max_week_prev].iloc[0, 0]
 
-    return max_week_number
+    max_week_number = str(year) + str(max_week_number)
+
+    return year, month, max_week_number, curr_week
